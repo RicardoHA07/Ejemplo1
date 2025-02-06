b3c270356a9012c0773c804536036eb48cb0100e
Ricardo Huitron <ricardoharaiz@gmail.com>
2025-02-06 13:44:25 -0600

    Programa Base Modificaciones para la clase del dia 6 de febrero del 2025

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.ejemplo1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -11,49 +13,44 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Ejemplo1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-
-                ) {
-                }
+            Ejemplo1Theme{
+                    Content()
             }
         }
     }
 
-
-    private fun Surface(modifier: Modifier, color: Any, function: () -> Unit) {
+    @Preview(showBackground = true)
+    @Composable
+    fun Content(){
+        Text(
+            text = "Hola Jetpack Compose",
+            color = Color.Blue,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding()
+                .background(Color.Blue)
+        )
+    /*Modificadores
+    * De posicionamiento = width, height, top, bottom, start, end
+    * De funcionalidad = click, scroll
+    * De apariencia = background, padding, border
+    *
+     */
     }
 }
 
-@Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Text(
-        text = from,
 
-    )
-
-    Text(
-        text = message,
-        fontSize  = 36.sp
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Ejemplo1Theme {
-        GreetingText(message = "Feliz cumple!", from = "De Ricardo H")
-
-    }
-}
