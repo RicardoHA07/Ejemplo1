81a0d89825a5ca7f464ae8bf419cb692c65f9119
Ricardo Huitron <ricardoharaiz@gmail.com>
2025-02-17 13:47:14 -0600

    Se crea rama de imagenes 17 de febrero 2025

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -5,18 +5,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.TopCenter
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
@@ -26,31 +40,79 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Ejemplo1Theme{
-                    Content()
+                    GreetingPreview()
             }
         }
     }
 
     @Preview(showBackground = true)
     @Composable
-    fun Content(){
-        Text(
-            text = "Hola Jetpack Compose",
-            color = Color.Blue,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            textAlign = TextAlign.Center,
+    fun GreetingPreview(){
+        Box(
             modifier = Modifier
-                .padding()
-                .background(Color.Blue)
-        )
-    /*Modificadores
-    * De posicionamiento = width, height, top, bottom, start, end
-    * De funcionalidad = click, scroll
-    * De apariencia = background, padding, border
-    *
-     */
+                .fillMaxSize(),
+            contentAlignment = TopCenter
+        ){
+        //SimpleImage()
+        //CircleImageView()
+        RoundCornerImageView()
+        //ImageWithBackgroundColor()
+        //ImageWithTint()
+
     }
+    }
+
+    @Composable
+    fun SimpleImage() {
+        Image(
+            painter = painterResource(id = R.drawable.andy_rubin),
+            contentDescription = "Andy Rubin",
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+@Composable fun CircleImageView() {
+    Image( painter = painterResource(R.drawable.andy_rubin),
+        contentDescription = "Circle Image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier .size(128.dp) .clip(CircleShape)
+            .border(5.dp, Color.Gray, CircleShape),
+        alignment = Alignment.Center,
+    )
+
+}
+@Composable
+fun RoundCornerImageView() {
+    Image(
+        painter = painterResource(R.drawable.andy_rubin),
+        contentDescription = "Round corner image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(RoundedCornerShape(10))
+            .border(5.dp, Color.Gray, RoundedCornerShape(10))
+    )
 }
 
+@Composable
+fun ImageWithBackgroundColor() {
+    Image(
+        painter = painterResource(id = R.drawable.carrito),
+        contentDescription = "",
+        modifier = Modifier
+            .size( 200.dp)
+            .background(Color.LightGray)
+            .padding(20.dp)
+    )
+}
 
+@Composable
+fun ImageWithTint() {
+
+    Image(
+        painter = painterResource(id = R.drawable.carrito),
+        contentDescription = "",
+        colorFilter = ColorFilter.tint(Color.Red),
+        modifier = Modifier
+            .size( 200.dp)
+    )
+}
