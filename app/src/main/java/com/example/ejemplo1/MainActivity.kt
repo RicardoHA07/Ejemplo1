c6c4e2d198d3adb9a3bba530ce5a19e827c88e31
Ricardo Huitron <ricardoharaiz@gmail.com>
2025-02-19 12:27:49 -0600

    Practica 2 18 de febrero

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -5,18 +5,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
@@ -33,24 +49,69 @@ class MainActivity : ComponentActivity() {
 
     @Preview(showBackground = true)
     @Composable
-    fun Content(){
-        Text(
-            text = "Hola Jetpack Compose",
-            color = Color.Blue,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding()
-                .background(Color.Blue)
-        )
-    /*Modificadores
-    * De posicionamiento = width, height, top, bottom, start, end
-    * De funcionalidad = click, scroll
-    * De apariencia = background, padding, border
-    *
-     */
-    }
-}
+    fun Content() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SimpleImage()
 
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OutLineTextFieldSample()
+                OutLineTextFieldSample2()
+                ButtonWithRoundCornerShape()
+            }
+        }
+    }
+
+    @Composable
+    fun SimpleImage() {
+        Image(
+            painter = painterResource(id = R.drawable.back),
+            contentDescription = "Fondo",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
+
+    @Composable
+    fun OutLineTextFieldSample() {
+        var text by remember { mutableStateOf(TextFieldValue("")) }
+        OutlinedTextField(
+            value = text,
+            label = { Text(text = "Valor 1") },
+            onValueChange = {
+                text = it
+            }
+        )
+    }
+    @Composable
+    fun OutLineTextFieldSample2() {
+        var text by remember { mutableStateOf(TextFieldValue("")) }
+        OutlinedTextField(
+            value = text,
+            label = { Text(text = "Valor 2") },
+            onValueChange = {
+                text = it
+            }
+        )
+    }
+
+
+        @Composable
+        fun ButtonWithRoundCornerShape() {
+            Button(onClick = {}, shape = RoundedCornerShape(20.dp)) {
+                Text(text = "Calcular")
+            }
+        }
+
+
+    }
 
