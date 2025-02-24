16818d664526a4f8fa0f0479a7a1377d68cb964c
Ricardo Huitron <ricardoharaiz@gmail.com>
2025-02-24 13:55:25 -0600

    Practica 3 de 21 de febrero

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -19,38 +19,25 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
+import com.example.ejemplo1.ui.theme.views.DetailView
+import com.example.ejemplo1.ui.theme.views.HomeView
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            Ejemplo1Theme{
-                    Content()
+          Content()
             }
         }
     }
 
     @Preview(showBackground = true)
     @Composable
-    fun Content(){
-        Text(
-            text = "Hola Jetpack Compose",
-            color = Color.Blue,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding()
-                .background(Color.Blue)
-        )
-    /*Modificadores
-    * De posicionamiento = width, height, top, bottom, start, end
-    * De funcionalidad = click, scroll
-    * De apariencia = background, padding, border
-    *
-     */
+    fun Content() {
+    HomeView()
     }
-}
+
 
 
---
+++ app/src/main/java/com/example/ejemplo1/ui/theme/views/DetailView.kt
@@ -0,0 +1,44 @@
+package com.example.ejemplo1.ui.theme.views
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailView(){
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "Detail view", color = Color.White) },
+                colors = androidx.compose.material3.TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.Blue
+                )
+            )
+        }
+    )
+    {
+        ContentView()
+    }
+}
+
+@Composable
+private fun ContentView() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Home")
+    }
+}
